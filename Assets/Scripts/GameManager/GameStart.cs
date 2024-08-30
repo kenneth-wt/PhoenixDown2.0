c2e3104c0dd2d8c0e4f620b57509e1413bca35c8
kenneth-wt <kennethtjandra7@gmail.com>
2024-08-30 16:38:50 +1000

    Last_changeb4_ScrumW6

--- Assets/Scripts/GameManager/GameStart.cs
+++ Assets/Scripts/GameManager/GameStart.cs
@@ -3,80 +3,115 @@ using UnityEngine;
 
 public class GameStart : MonoBehaviour
 {
-  public BlackFade blackFadeScript;
-  public GameObject cryptDoor;
-  public GameObject[] tombstones;
-  public GameObject wellPopper;
-  public GameObject ball;
-  public GameObject tiltManager;
+    public BlackFade blackFadeScript;
+    public GameObject cryptDoor;
+    public GameObject[] tombstones;
+    public GameObject wellPopper;
+    public GameObject ball;
+    public GameObject tiltManager;
 
-  public GameObject[] rollovers;
-  private Collider cryptDoorCollider;
-  
-  void Awake()
-  {
-    switch (SceneArguments.previousSceneCondition)
+    public GameObject[] rollovers;
+    private Collider cryptDoorCollider;
+    private bool isPaused = false;
+
+    void Awake()
     {
-      case "B3B2Loss":
-        Physics.gravity = new Vector3(0, Constants.gravityY, Constants.gravityZ);
-        foreach (GameObject tombstone in tombstones)
+        switch (SceneArguments.previousSceneCondition)
         {
-          tombstone.GetComponent<RegisterTargetHit>().hit = true;
+            case "B3B2Loss":
+                Physics.gravity = new Vector3(0, Constants.gravityY, Constants.gravityZ);
+                foreach (GameObject tombstone in tombstones)
+                {
+                    tombstone.GetComponent<RegisterTargetHit>().hit = true;
+                }
+
+                ball.GetComponent<Rigidbody>().isKinematic = true;
+
+                cryptDoorCollider = cryptDoor.GetComponent<Collider>();
+                cryptDoorCollider.enabled = false;
+
+                ball.GetComponent<Rigidbody>().isKinematic = true;
+                ball.transform.position = cryptDoorCollider.bounds.center;
+                StartCoroutine(WaitToKick());
+                break;
+            case "B3B2Win":
+                Physics.gravity = new Vector3(0, Constants.gravityY, Constants.gravityZ);
+                foreach (GameObject tombstone in tombstones)
+                {
+                    tombstone.GetComponent<RegisterTargetHit>().hit = true;
+                }
+
+                ball.GetComponent<Rigidbody>().isKinematic = true;
+
+                ball.transform.position = wellPopper.GetComponent<Collider>().bounds.center;
+
+                foreach (GameObject rollover in rollovers)
+                {
+                    rollover.SetActive(true);
+                }
+
+                tiltManager.GetComponent<Tilt>().enabled = false;
+
+                StartCoroutine(WaitToPop());
+                break;
+            default:
+                break;
         }
 
-        ball.GetComponent<Rigidbody>().isKinematic = true;
-
-        cryptDoorCollider = cryptDoor.GetComponent<Collider>();
-        cryptDoorCollider.enabled = false;
-
-        ball.GetComponent<Rigidbody>().isKinematic = true;
-        ball.transform.position = cryptDoorCollider.bounds.center;
-        StartCoroutine(WaitToKick());
-        break;
-      case "B3B2Win":
-        Physics.gravity = new Vector3(0, Constants.gravityY, Constants.gravityZ);
-        foreach (GameObject tombstone in tombstones)
-        {
-          tombstone.GetComponent<RegisterTargetHit>().hit = true;
-        }
-        
-        ball.GetComponent<Rigidbody>().isKinematic = true;
-
-        ball.transform.position = wellPopper.GetComponent<Collider>().bounds.center;
-
-        foreach (GameObject rollover in rollovers)
-        {
-          rollover.SetActive(true);
-        }
-
-        tiltManager.GetComponent<Tilt>().enabled = false;
-
-        StartCoroutine(WaitToPop());
-        break;
-      default:
-        break;
+        blackFadeScript.StartFade(false);
     }
 
-    blackFadeScript.StartFade(false);
-  }
+    void Update()
+    {
+        // Handle the game pause/resume functionality
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            TogglePauseGame();
+        }
+    }
 
-  IEnumerator WaitToKick()
-  {
-    yield return new WaitForSeconds(Constants.fadeDuration + Constants.fadeBuffer);
-    ball.GetComponent<Rigidbody>().isKinematic = false;
+    private void TogglePauseGame()
+    {
+        if (isPaused)
+        {
+            ResumeGame();
+        }
+        else
+        {
+            PauseGame();
+        }
+    }
 
-    CryptDoor cryptDoorScript = cryptDoor.GetComponent<CryptDoor>();
-    cryptDoor.GetComponent<Collider>().enabled = true;
+    public void PauseGame()
+    {
+        Time.timeScale = 0f; // Pause the game
+        isPaused = true;
+        // Optionally, show the pause menu or UI here
+    }
 
+    public void ResumeGame()
+    {
+        Time.timeScale = 1f; // Resume the game
+        isPaused = false;
+        // Optionally, hide the pause menu or UI here
+    }
 
-    yield return new WaitForSeconds(cryptDoorScript.cooldownTime);
-    cryptDoorScript.unlocked = true;
-  }
+    IEnumerator WaitToKick()
+    {
+        yield return new WaitForSeconds(Constants.fadeDuration + Constants.fadeBuffer);
+        ball.GetComponent<Rigidbody>().isKinematic = false;
 
-  IEnumerator WaitToPop()
-  {
-    yield return new WaitForSeconds(Constants.fadeDuration + Constants.fadeBuffer);
-  
-    wellPopper.GetComponent<WellPopper>().BallToFountains();
-  }
+        CryptDoor cryptDoorScript = cryptDoor.GetComponent<CryptDoor>();
+        cryptDoor.GetComponent<Collider>().enabled = true;
+
+        yield return new WaitForSeconds(cryptDoorScript.cooldownTime);
+        cryptDoorScript.unlocked = true;
+    }
+
+    IEnumerator WaitToPop()
+    {
+        yield return new WaitForSeconds(Constants.fadeDuration + Constants.fadeBuffer);
+
+        wellPopper.GetComponent<WellPopper>().BallToFountains();
+    }
 }
